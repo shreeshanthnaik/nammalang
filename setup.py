7a02fe22c97b5ba54abe71b5a700b91e5d86836b
shreeshanthnaik <92677430+shreeshanthnaik@users.noreply.github.com>
2025-10-08 10:58:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="nammalang",
     version="0.1.0",
-    author="Your Name",
+    author="Shreeshanth",
     author_email="shreeshanthnaik@gmail.com",
     description="A Kannada-inspired programming language.",
     packages=find_packages(),
